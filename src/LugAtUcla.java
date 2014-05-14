d96b4f3f318204ce7e94f6a370ffbfd37ffd78f2
Andy Spencer <andy753421@gmail.com>
2014-05-14 22:55:06 +0000

    Add more complete user interface
    Our user interface consists of a top section showing the coffee maker status
    using icons and text view, a text status area, and a refresh button.
    
        +-----------------------------------------+
        |                                         |
        | [*] Pot In    May 14, 2014 10:30:00 AM  |
        | [ ] Lid Open  May 14, 2014 10:00:00 AM  |
        |                                         |
        |---------------------------------------- |
        |                                         |
        | Loading...                              |
        |                                         |
        |                                         |
        |                                         |
        |                                         |
        |+---------------------------------------+|
        ||         Refresh Coffee Status         ||
        |+---------------------------------------+|
        +-----------------------------------------+

--- src/LugAtUcla.java
+++ src/LugAtUcla.java
@@ -1,7 +1,14 @@
 package edu.ucla.linux.tutorial;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
 
 /*
  * Our Main Activity (the user interface). This is launched when the user
@@ -11,6 +18,66 @@ import android.os.Bundle;
  */
 public class LugAtUcla extends Activity
 {
+    /* Activity Widgets */
+    private TextView  statusView;  // Text view for dumping ASCII status
+                                   // This also shows error messages
+
+    private ImageView potIn;       // Star icon for pos status
+    private TextView  potActivity; // Pot activity timestamp field
+    private ImageView lidOpen;     // Star icon for lid status
+    private TextView  lidActivity; // Lid activity timestamp field
+
+    /* Display the coffee status to the user */
+    private void setStatus(CoffeeStatus status)
+    {
+        DateFormat fmt = DateFormat.getDateTimeInstance();
+        int        yes = android.R.drawable.star_on;
+        int        no  = android.R.drawable.star_off;
+
+        // Update graphical display 
+        //   this provides a nice, readable representation of the coffee
+        //   status, but it does not support showing error messages so we only
+        //   update it if there have been no errors.
+        if (status.error == null) {
+            this.potIn.setImageResource(status.potIn ? yes : no);
+            this.potActivity.setText(fmt.format(status.potActivity));
+            this.lidOpen.setImageResource(status.lidOpen ? yes : no);
+            this.lidActivity.setText(fmt.format(status.lidActivity));
+        }
+
+        // Set text view contents
+        //   normally this shows the same information as the graphical display,
+        //   but if there are errors, we will display them here.
+        statusView.setTypeface(Typeface.MONOSPACE);
+        statusView.setText(status.toString());
+    }
+
+    /* Trigger a refresh of the coffee pot status */
+    private void loadStatus()
+    {
+        // Notify the user that we're loading
+        statusView.setText("Loading..");
+
+        // Update status with dummy data
+        CoffeeStatus status = new CoffeeStatus();
+        status.potIn   = Math.random() > 0.5;
+        status.lidOpen = Math.random() > 0.5;
+        this.setStatus(status);
+        Toast.makeText(LugAtUcla.this, "Load complete", Toast.LENGTH_SHORT).show();
+    }
+
+    /* Callbacks - there are multiple ways to specify callback but one of the
+     *             easiest ones is to add the onClick attribute to
+     *             res/layout/main.xml with a function to call when the user
+     *             presses the button.
+     *
+     *             Callback methods assigned in this way must be public and
+     *             have the correct argument types. */
+    public void onRefresh(View btn)
+    {
+        this.loadStatus();
+    }
+
     /* Initialize our app
      *   The "lifecycle" of an Android App is much more complex than this, but
      *   luckily we can do everything in onCreate because we don't have to save
@@ -21,5 +88,15 @@ public class LugAtUcla extends Activity
 
         // Load the user interfaces from the XML UI description
         setContentView(R.layout.main);
+
+        // Lookup all our user interface widgets
+        this.statusView  =  (TextView)findViewById(R.id.status);
+        this.potIn       = (ImageView)findViewById(R.id.pot_in);
+        this.potActivity =  (TextView)findViewById(R.id.pot_activity);
+        this.lidOpen     = (ImageView)findViewById(R.id.lid_open);
+        this.lidActivity =  (TextView)findViewById(R.id.lid_activity);
+
+        // Trigger the initial status update
+        this.loadStatus();
     }
 }
